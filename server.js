e8f81d82b79a078d8e0afeddd9a20dc75265ca9b
Gourav-Ydv <90626117+Gourav-Ydv@users.noreply.github.com>
2024-05-03 14:51:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Redirect route
-app.get('/', (req, res) => {
-  res.redirect('https://www.google.com/'); // Replace with your desired URL
+// app.get('/', (req, res) => {
+//   res.redirect('https://www.google.com/'); // Replace with your desired URL
+// });
+
+app.get('/AngelOne', (req, res) => {
+  res.redirect('https://www.angelone.in/register/#/login'); // Replace with your desired URL
 });
+
+app.get('/MStock', (req, res) => {
+  res.redirect('https://www.mstock.com/open-demat-account'); // Replace with your desired URL
+});
+
 
 // Start server
 app.listen(PORT, () => {
